3b2518c9dfafa32824c694bf7e991e39b8922a90
Brian Bodiya <brian@bodiya.com>
2014-09-04 22:21:20 -0400

    updated email address

--- transmitters/ATmega328P_DHT11/ATmega328P_DHT11.ino
+++ transmitters/ATmega328P_DHT11/ATmega328P_DHT11.ino
@@ -18,7 +18,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 For questions, comments, or suggestions about this software,
-you can contact Brian Bodiya at: ***REMOVED***
+you can contact Brian Bodiya at: brian@bodiya.com
 *************************************************************************/
 
 #include <avr/sleep.h>
